27ddba22ae7046b13046597cafc0a7d6b1f42850
bhangun <bhangunh@gmail.com>
2021-09-14 09:28:51 +0700

    add license

--- core/utils.js
+++ core/utils.js
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2013-2021 the original author or authors Bhangun Hartani
+ * This file is part of the Kujang Generator
+ * for more information.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const _ = require('lodash');
 const SwaggerParser = require("@apidevtools/swagger-parser");
 
@@ -204,7 +222,6 @@ function transformType(type, isEnum) {
 
   return newType
 }
-
 
 
 /**
@@ -503,7 +520,12 @@ function propsForServices(paths) {
   return methods
 }
 
-
+/**
+ * 
+ * @param {*} input 
+ * @param {*} resType 
+ * @returns 
+ */
 function putParam(input, resType) {
   let result = '';
   let param = {}
@@ -567,7 +589,11 @@ function putParam(input, resType) {
   };
 }
 
-
+/**
+ * 
+ * @param {*} m 
+ * @returns 
+ */
 function validatePath(m) {
   let method = m;
   if (m == 'put')
@@ -578,7 +604,11 @@ function validatePath(m) {
   return method;
 }
 
-
+/**
+ * 
+ * @param {*} paths 
+ * @returns 
+ */
 function otherEntity(paths) {
   const responseTypes = [];
   for (const i in paths) {
@@ -614,6 +644,11 @@ function otherEntity(paths) {
   return responseTypes
 }
 
+/**
+ * 
+ * @param {*} input 
+ * @returns 
+ */
 function otherFields(input) {
   let param = {}
   const fields = []
